4e3349465935d5186453212be33c10f670981342
Mwangolé Estudante <mwangoleestudante@gmail.com>
2026-03-29 19:32:19 +0100

    segundo teste

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ function adicionarPonto() {
     pontos++;
     document.getElementById('pontos').innerText = pontos;
 }
+function mostrarSecao(idDaSecao) {
+    // 1. Seleciona todas as sections do site
+    let secoes = document.querySelectorAll('section');
+
+    // 2. Esconde todas elas uma por uma
+    secoes.forEach(s => {
+        s.style.display = 'none';
+    });
+
+    // 3. Mostra apenas aquela que tem o ID que clicámos
+    document.getElementById(idDaSecao).style.display = 'block';
+}
